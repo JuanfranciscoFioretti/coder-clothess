c463644fca9f6edfe3f79ce008f5031fbfa0266c
Juan Francisco Fioretti <juanfrafio125@gmail.com>
2022-09-05 01:07:54 -0300

    Commit de actualizacion del nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,17 +6,23 @@ const NavBar = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">Cookit</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0" id='NavUl'>
                             <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" id='HomeNav' aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active" href="#">Link</a>
+                                <a className="nav-link active" href="#">Cooking</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Utensils</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Holders</a>
                             </li>
                             <li className="nav-item">
                                 {/* <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
